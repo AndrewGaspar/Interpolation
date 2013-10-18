a9430cb20dbba19fb81963b92760dfc49634797f
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-10-18 18:15:08 -0500

    Interpolation works now.

---
+++ Interpolation/Interpolation.fs
@@ -0,0 +1,18 @@
+﻿namespace NumericalAnalysis
+
+module Interpolation =
+    let interpolate (dataPoints: seq<float * float>) =
+        let data = dataPoints |> Seq.toArray
+        let rec dividedDifference i k = 
+            if i = k then (snd data.[i]);
+            else 
+                ((dividedDifference (i+1) k) - (dividedDifference i (k-1))) 
+                    / (fst data.[k] - fst data.[i])
+
+        let a n = dividedDifference 0 n
+        
+        let rec p n =
+            if n = data.Length - 1 then (fun x -> a n)
+            else (fun x -> (x - fst data.[n])*(p (n+1) x) + a n)
+
+        p 0
---
+++ Interpolation/Tests.fsx
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Interpolation.fs"
+open NumericalAnalysis.Interpolation
+open System
+
+let interpolatedSin =
+    seq { for i in 0..8 -> float i * Math.PI / 2.0 } 
+        |> Seq.map (fun x -> (x, Math.Sin x)) 
+        |> interpolate
+
+seq { for i in 0..64 -> float i * Math.PI / 32.0 } 
+    |> Seq.map (fun x -> (x, interpolatedSin x, Math.Sin x))
+    |> Seq.iter (fun (x,x1,x2) -> printfn "I(%f) = %f | S(%f) = %f | |I - S| = %f" x x1 x x2 (Math.Abs (x1 - x2)))
