699fae73ec80d51ace5e474319af4be5522e1234
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-10-21 16:20:19 -0500

    Added a nuspec for create a nuget package.

--- Interpolation/P2.fsx
+++ Interpolation/P2.fsx
@@ -18,3 +18,21 @@ part2Data
 part2Data
 |> hermiteEquation 
 |> printfn "%s"
+
+let func =
+    part2Data
+    |> hermite
+
+func 10.0 |> printfn "%f"
+
+let part2CData = 
+    [   
+        (0.0,   75.0); 
+        (3.0,   77.0); 
+        (5.0,   80.0);
+        (8.0,   74.0);
+        (13.0,  72.0)
+    ]   
+    |> Seq.map (fun (x,v) -> (x,v*3600.0/5280.0));
+
+let v = part2CData |> interpolate
